4029daeb8a097caf8ef67a4a9d0019a7ef472ab8
vineetsouri <vineetsouri72@gmail.com>
2016-12-12 12:00:07 +0530

    included  firebase apikey and storage bucket key to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { MyApp } from './app.component';
 import { Page1 } from '../pages/resource/page1';
 import { Page2 } from '../pages/page2/page2';
 import { ResourceDetailsPage } from '../pages/resource-details/resource-details';
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB1uU4D27iEm54moCM_N1KS8vfOXTCmZbU',
+  authDomain: 'ionic2-f99e1.firebaseapp.com',
+  databaseURL: 'https://ionic2-f99e1.firebaseio.com',
+  storageBucket: 'ionic2-f99e1.appspot.com'
+};
+
+export const firebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
 
 @NgModule({
   declarations: [
@@ -13,7 +26,8 @@ import { ResourceDetailsPage } from '../pages/resource-details/resource-details'
     ResourceDetailsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, firebaseAuthConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
