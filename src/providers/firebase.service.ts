76383fadabc4a97cbfc39ae9937c662ed488572f
vineetsouri <vineetsouri72@gmail.com>
2016-12-22 13:08:28 +0530

    added date picker to activity details.

--- src/providers/firebase.service.ts
+++ src/providers/firebase.service.ts
@@ -8,18 +8,28 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 @Injectable()
 export class FireBaseService {
 
+  // masterActivities: Array<{}>;
+  public masterActivities$: FirebaseListObservable<any>;
+  public masterResources$: FirebaseListObservable<any>;
+  public projectActivities$: FirebaseListObservable<any>;
+  public activityResource$: FirebaseListObservable<any>;
+
   public usersList$: Observable<any>;
   constructor(private af: AngularFire) {
     console.log('In AF constructor');
+    this.fetchActivitiesList$();
+    this.fetchResourcesList$();
   }
 
 
   fetchActivitiesList$(): FirebaseListObservable<any> {
-    return this.af.database.list('/activitiesList') as FirebaseListObservable<any>;
+    this.masterActivities$ = this.af.database.list('/activitiesList') as FirebaseListObservable<any>;
+    return this.masterActivities$;
   }
 
   fetchResourcesList$(): FirebaseListObservable<any> {
-    return this.af.database.list('/resourcesList') as FirebaseListObservable<any>;
+    this.masterResources$ = this.af.database.list('/resourcesList') as FirebaseListObservable<any>;
+    return this.masterResources$;
   }
 
   fetchProjects$(): FirebaseListObservable<any> {
@@ -27,14 +37,24 @@ export class FireBaseService {
   }
 
   fetchProjectActivities$(projectId): FirebaseListObservable<any> {
-    return this.af.database.list('/activities', {
+    this.projectActivities$ = this.af.database.list('/activities', {
       query: {
         orderByChild: 'projectId',
         equalTo: projectId
       }
     }) as FirebaseListObservable<any>;
+    return this.projectActivities$;
   }
 
+  fetchActivityResources$(activityId): FirebaseListObservable<any> {
+    this.activityResource$ = this.af.database.list('/resources', {
+      query: {
+        orderByChild: 'activityId',
+        equalTo: activityId
+      }
+    }) as FirebaseListObservable<any>;
+    return this.activityResource$;
+  }
 
 
   logout(){
